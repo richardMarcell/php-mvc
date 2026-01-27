9de188a8f3a7c93583188baaa749bd0bf168d36d
Richard Marcell <richard.marcell8888@gmail.com>
2026-01-27 08:34:31 +0700

    feat: implement student controller

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Core;
 
+use App\Controllers\StudentController;
+
 class Router
 {
 
@@ -10,13 +12,21 @@ class Router
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         if ($method == 'GET' && $uri == '/students') {
-            echo '<h1>Daftar Siswa</h1>';
-            echo '<p>Menampilkan daftar siswa</p>';
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->index();
+            return;
+        }
+
+        if ($method == 'GET' && $uri == '/students/create') {
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->create();
             return;
         }
 
         http_response_code(404);
-        echo '<h1>404 - x`Page Not Found</h1>';
+        echo '<h1>404 - Page Not Found</h1>';
     }
 
 }
