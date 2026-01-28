0de0afa4f5dd8eb1c139f7abd18f10ccf42eadeb
Richard Marcell <richard.marcell8888@gmail.com>
2026-01-28 09:48:27 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Core;
 
+use App\Controllers\StudentController;
+
 class Router
 {
 
@@ -10,14 +12,16 @@ class Router
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         if ($method == 'GET' && $uri == '/students') {
-            echo '<h1>Daftar Siswa</h1>';
-            echo '<p>Menampilkan daftar siswa</p>';
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->index();
             return;
         }
 
         if ($method == 'GET' && $uri == '/students/create') {
-            echo '<h1>Tambah Siswa</h1>';
-            echo '<p>Menampilkan form tambah siswa</p>';
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->create();
             return;
         }
 
