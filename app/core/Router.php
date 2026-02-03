5da88ebefdb091853abf3ca1c632f27a3ea69332
Richard Marcell <richard.marcell8888@gmail.com>
2026-02-03 14:35:41 +0700

    feat: implement router app

--- app/core/Router.php
+++ app/core/Router.php
@@ -23,22 +23,28 @@ class Router
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 
-        foreach($this->routes as $route) {
-            
-        }
+        foreach ($this->routes as $route) {
+            $pattern = str_replace(
+                '{id}',
+                '([0-9]+)',
+                $route['uri'],
+            );
+            $pattern = '#^' . $pattern . '$#';
+            // /students/([0-9]+)
 
-        if ($method == 'GET' && $uri == '/students') {
-            require_once './app/controllers/StudentController.php';
-            $controller = new StudentController();
-            $controller->index();
-            return;
-        }
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches);
+                require_once './app/controllers/' . $route['controller'] . '.php';
 
-        if ($method == 'GET' && $uri == '/students/create') {
-            require_once './app/controllers/StudentController.php';
-            $controller = new StudentController();
-            $controller->create();
-            return;
+                $controllerClass = 'App\\Controllers\\' . $route['controller'];
+                $controller = new $controllerClass();
+                $function = $route['function'];
+
+                call_user_func_array([$controller, $function], $matches);
+                // shortcut: index($parameter1, $parameter2, dst);
+                // example: call_user_func_array(['StudentController', 'index'], [1,2])
+                return;
+            }
         }
 
         http_response_code(404);
