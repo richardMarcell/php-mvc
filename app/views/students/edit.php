ca1ed7f6a59a352382ceeb0f8b5e4fa31871fb7e
Richard Marcell <richard.marcell8888@gmail.com>
2026-02-23 11:37:20 +0700

    feat: setup tailwind

---
+++ app/views/students/edit.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Siswa</title>
+</head>
+
+<body>
+    <h1>Edit Siswa</h1>
+    <p>Menampilkan form edit siswa</p>
+</body>
+
+</html>
---
+++ app/views/students/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Siswa</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    <div>
+        <h1 class="text-red-500">Daftar Siswa</h1>
+        <p>Menampilkan daftar siswa</p>
+    </div>
+</body>
+
+</html>
