6e63e91ee3af9361bc515ce73642fc66d7fe89b5
Richard Marcell <richard.marcell8888@gmail.com>
2026-02-23 11:51:28 +0700

    feat: implemnent new tailwind

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?> - Sistem Sekolah</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="/css/output.css">
 </head>
 
 <body class="bg-gray-100 min-h-screen flex flex-col">
