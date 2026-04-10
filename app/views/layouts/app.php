ab69e5991b4a63ae14264e4388178beeaf440528
Richard Marcell <richard.marcell8888@gmail.com>
2026-04-10 10:55:35 +0700

    feat: implement layout

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -10,12 +10,7 @@
 
 <body class="min-h-screen flex flex-col bg-gray-100">
     <!-- Header Start -->
-    <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-4">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
-        </div>
-    </header>
+    <?php require_once '../app/views/layouts/partials/header.php' ?>
     <!-- Header End -->
 
     <!-- Main Start -->
@@ -25,12 +20,7 @@
     <!-- Main End -->
 
     <!-- Footer Start -->
-    <footer class="bg-gray-800 text-white">
-        <div class="text-center p-4">
-            &copy
-            <?= date('Y') ?> - Sistem Sekolah SMK Kristen Immanuel
-        </div>
-    </footer>
+    <?php require_once '../app/views/layouts/partials/footer.php' ?>
     <!-- Footer End -->
 </body>
 
--- app/views/students/create.php
+++ app/views/students/create.php
@@ -1,74 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="mt-8 space-y-4">
+    <!-- Card Header Start -->
+    <div class="bg-white shadow rounded-lg p-4">
+        <h1 class="text-2xl font-bold">Tambah Siswa</h1>
+        <p>Menambahkan siswa ke dalam sistem</p>
+    </div>
+    <!-- Card Header End -->
 
-<head>
-    <meta chaarset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Siswa</title>
-    <link rel="stylesheet" href="/css/output.css">
-</head>
-
-<body class="min-h-screen flex flex-col bg-gray-100">
-    <!-- Header Start -->
-    <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-4">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
-        </div>
-    </header>
-    <!-- Header End -->
-
-    <!-- Main Start -->
-    <main class="grow container mx-auto">
-        <div class="mt-8 space-y-4">
-            <!-- Card Header Start -->
-            <div class="bg-white shadow rounded-lg p-4">
-                <h1 class="text-2xl font-bold">Tambah Siswa</h1>
-                <p>Menambahkan siswa ke dalam sistem</p>
+    <!-- Card Content Start -->
+    <div class="bg-white shadow rounded-lg">
+        <form action="" class="p-4 grid grid-cols-2 gap-4">
+            <div class="space-y-2">
+                <label class="block font-bold" for="name">Nama</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name"
+                    placeholder="Masukkan nama">
             </div>
-            <!-- Card Header End -->
-
-            <!-- Card Content Start -->
-            <div class="bg-white shadow rounded-lg">
-                <form action="" class="p-4 grid grid-cols-2 gap-4">
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="name">Nama</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name"
-                            placeholder="Masukkan nama">
-                    </div>
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="nis">NIS</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis"
-                            placeholder="Masukkan NIS">
-                    </div>
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="class">Kelas</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="class" id="class"
-                            placeholder="Masukkan Kelas">
-                    </div>
-                    <div class="space-y-2">
-                        <label class="block font-bold" for="phone_number">No Telepon</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number"
-                            id="phone_number" placeholder="Masukkan No Telepon">
-                    </div>
-                    <div class="flex justify-end gap-4 col-span-2">
-                        <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
-                        <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
-                    </div>
-                </form>
+            <div class="space-y-2">
+                <label class="block font-bold" for="nis">NIS</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis"
+                    placeholder="Masukkan NIS">
             </div>
-            <!-- Card Content End -->
-        </div>
-    </main>
-    <!-- Main End -->
-
-    <!-- Footer Start -->
-    <footer class="bg-gray-800 text-white">
-        <div class="text-center p-4">
-            &copy <?= date('Y') ?> - Sistem Sekolah SMK Kristen Immanuel
-        </div>
-    </footer>
-    <!-- Footer End -->
-</body>
-
-</html>
+            <div class="space-y-2">
+                <label class="block font-bold" for="class">Kelas</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="class" id="class"
+                    placeholder="Masukkan Kelas">
+            </div>
+            <div class="space-y-2">
+                <label class="block font-bold" for="phone_number">No Telepon</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number"
+                    placeholder="Masukkan No Telepon">
+            </div>
+            <div class="flex justify-end gap-4 col-span-2">
+                <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+                <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
+            </div>
+        </form>
+    </div>
+    <!-- Card Content End -->
+</div>
