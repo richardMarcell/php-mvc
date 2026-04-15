604c659023fde31dfeef61a0a523a803714fb0da
Richard Marcell <richard.marcell8888@gmail.com>
2026-04-15 10:15:09 +0700

    feat: implement student detail page

--- app/models/Student.php
+++ app/models/Student.php
@@ -26,6 +26,21 @@ class Student extends Database
         return $students;
     }
 
+    // Fungsi Untuk Menampilkan Detail Siswa
+    public function getStudent(int $id)
+    {
+        $query = "SELECT * FROM {$this->table} WHERE id = ?";
+
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $student = $result->fetch_assoc();
+
+        return $student;
+    }
 }
 
 ?>
--- app/views/students/show.php
+++ app/views/students/show.php
@@ -11,23 +11,23 @@
         <div class="p-4 grid grid-cols-2 gap-4">
             <div class="space-y-2">
                 <label class="font-bold block" for="name">Nama</label>
-                <input class="px-4 py-2 border rounded-lg w-full" type="text" id="name" name="name"
-                    placeholder="Masukkan nama" readonly>
+                <input value="<?= $student['name'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text" id="name"
+                    name="name" placeholder="Masukkan nama" readonly>
             </div>
             <div class="space-y-2">
                 <label class="font-bold block" for="nis">NIS</label>
-                <input class="px-4 py-2 border rounded-lg w-full" type="text" id="nis" name="nis"
-                    placeholder="Masukkan NIS" readonly>
+                <input value="<?= $student['nis'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text" id="nis"
+                    name="nis" placeholder="Masukkan NIS" readonly>
             </div>
             <div class="space-y-2">
                 <label class="font-bold block" for="class">Kelas</label>
-                <input class="px-4 py-2 border rounded-lg w-full" type="text" id="class" name="class"
-                    placeholder="Masukkan kelas" readonly>
+                <input value="<?= $student['class'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text"
+                    id="class" name="class" placeholder="Masukkan kelas" readonly>
             </div>
             <div class="space-y-2">
                 <label class="font-bold block" for="phone_number">No Telepon</label>
-                <input class="px-4 py-2 border rounded-lg w-full" type="text" id="phone_number" name="phone_number"
-                    placeholder="Masukkan no telepon" readonly>
+                <input value="<?= $student['phone_number'] ?>" class="px-4 py-2 border rounded-lg w-full" type="text"
+                    id="phone_number" name="phone_number" placeholder="Masukkan no telepon" readonly>
             </div>
             <div class="flex justify-end gap-4 col-span-2">
                 <a href="/students" class="px-4 py-2 rounded-lg bg-gray-100">Kembali</a>
