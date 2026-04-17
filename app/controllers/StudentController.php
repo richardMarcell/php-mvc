8031a8e72252afe93556e88d0df02fa7f1a9a5c2
Richard Marcell <richard.marcell8888@gmail.com>
2026-04-17 11:08:15 +0700

    feat: implement student index page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -14,9 +14,9 @@ class StudentController extends Controller
         $studentModel = new Student();
         $students = $studentModel->getStudents();
 
-        print_r($students);
-
-        $this->view('students.index');
+        $this->view('students.index', [
+            'students' => $students
+        ]);
     }
 
     public function create()
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,6 +6,7 @@ class Controller
 
     public function view(string $view, array $data = [])
     {
+        extract($data);
         $view = str_replace(
             '.',
             '/',
