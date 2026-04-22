9d14b2a336e51dac6ddd58fe7eb88d5a2d01efa5
Richard Marcell <richard.marcell8888@gmail.com>
2026-04-22 09:12:19 +0700

    wip: implement student edit and delete

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -47,4 +47,18 @@ class StudentController extends Controller
         $studentModel->insert($_POST);
     }
 
+    public function update(string $id)
+    {
+        $id = intval($id);
+        $studentModel = new Student();
+        $studentModel->update($_POST, $id);
+    }
+
+    public function destroy(string $id)
+    {
+        $id = intval($id);
+        $studentModel = new Student();
+        $studentModel->delete($id);
+    }
+
 }
